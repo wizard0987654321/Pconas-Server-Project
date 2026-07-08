69091324cad629275b9ad11cfcd50444c0e4a3e7
Givi Sabashvili <givi.sabashvili@pcon.as>
2026-07-08 15:51:18 +0200

    dynamic room data load from backend added

--- frontend/src/components/ServerRoom.tsx
+++ frontend/src/components/ServerRoom.tsx
@@ -1,26 +1,54 @@
 import serverIcon from "../assets/ServerIcon.svg"
 import PrimaryButton from "./PrimaryButton";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { getRoomData } from "../services/api";
+import { useState, useEffect } from "react";
+
+type Room = {
+    ID: number,
+    Area: number,
+    Capacity: number,
+    HeightCm: number
+}
 
 function ServerRoom() {
     const navigate = useNavigate();
     const { t } = useTranslation();
 
+    const [roomsData, setroomsData] = useState<Room[]>([]);
+
+    //data[0].ID/Area/Capacity/HeightCm
+
+    useEffect(() => {
+        getRoomData()
+            .then((data) => {
+                console.log("API Response is sqlistvis:", data);
+                setroomsData(data);
+            })
+            .catch((err) => {
+                console.error("API erroria aee:", err);
+            });
+    }, []);
 
     return (
         <>
-            <h1>Room 1</h1>
-            <div className="flex flex-col items-center border-4 border-solid border-[#6ADBAF] rounded-[10px]">
-                <div className="grid grid-cols-2 grid-rows-2">
-                    <img
-                        src={serverIcon}
-                        className=""
-                        alt="Server Icon"
-                    />
+            {roomsData.map((room) => (
+                <div key={room.ID} className="flex flex-col items-center p-4">
+                    <h1>Room 1</h1>
+                    <div className="flex flex-col items-center border-4 border-solid border-[#6ADBAF] rounded-[10px]">
+                        <div
+                            className="grid grid-rows-2"
+                            style={{
+                                gridTemplateColumns: `repeat(${Math.ceil(room.Capacity / 2)}, minmax(0, 1fr))`,
+                            }}
+                        >
+                            <img src={serverIcon} alt="Server Icon" />
+                        </div>
+                        <PrimaryButton label={t("pages.rooms.button")} onClick={() => navigate("/racks")} />
+                    </div>
                 </div>
-                <PrimaryButton label={t("pages.rooms.button")} onClick={() => navigate("/racks")} />
-            </div>
+            ))}
         </>
     )
 }
