ac2c77d02f1b5800a5d34c806ab3bdac22dba5fd
Givi Sabashvili <givi.sabashvili@pcon.as>
2026-07-14 00:08:53 +0200

    pageHeading small css bug fixed

--- frontend/src/pages/ServicesPage.tsx
+++ frontend/src/pages/ServicesPage.tsx
@@ -3,7 +3,7 @@ import ServerServices from "../components/ServerServices";
 
 function ServicesPage() {
     return (
-        <div>
+        <div className="flex flex-col justify-center flex-wrap items-center p-4 w-full">
             <PageHeading heading="pages.services.headingText" />
             <ServerServices />
         </div>
--- frontend/src/pages/VmsPage.tsx
+++ frontend/src/pages/VmsPage.tsx
@@ -3,7 +3,7 @@ import ServerVms from "../components/ServerVms";
 
 function VmsPage() {
     return (
-        <div>
+        <div className="flex flex-col justify-center flex-wrap items-center p-4 w-full">
             <PageHeading heading="pages.vms.headingText" />
             <ServerVms />
         </div>
