b866dce04c84ae75ffac76eac4441096140ea98d
Givi Sabashvili <givi.sabashvili@pcon.as>
2026-07-24 23:44:35 +0200

    trainer/trainee separation logic added

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,6 @@
 import ThemeToggle from "./toggleButtons/ThemeToggle"
 import LanguageToggle from "./toggleButtons/LanguageToggle"
 import logo from "../assets/pconasLogo.png"
-import darkLogo from "../assets/pconasLogoDark.png"
 import { Link } from "react-router-dom"
 import { getDisplayLabel, useAuth } from "../contexts/AuthProvider"
 import { useTranslation } from "react-i18next"
@@ -12,23 +11,17 @@ function Header() {
 
     return (
         <>
-            <header className="h-[15vh] w-full bg-[#F8FAFC] border-4 border-[#6ADBAF] flex items-center justify-between dark:bg-[#0E1F48] l:px-4">
+            <header className="h-[15vh] w-full bg-[#F8FAFC] dark:bg-gray-900 border-4 border-[#6ADBAF] dark:border-[#3f8c6f] flex items-center justify-between l:px-4">
                 <Link to="/">
                     <img
                         src={logo}
                         alt="Logo"
-                        className="h-full max-h-12 w-auto scale-50 m:scale-75 l:scale-100 cursor-pointer dark:hidden"
-                    />
-
-                {/* rendering logo for dark mode */}
-                    <img
-                        src={darkLogo}
-                        alt="Logo"
-                        className="hidden h-full max-h-12 w-auto scale-50 m:scale-75 l:scale-100 cursor-pointer dark:block"
+                        className="h-full max-h-12 w-auto scale-50 m:scale-75 l:scale-100 cursor-pointer"
                     />
                 </Link>
-                <span className="hidden m:inline font-mono text-3xl font-bold text-[#111827] dark:text-[#F9FAFB]">
-                    Trainer
+                <span className="hidden m:flex flex-col items-center font-mono text-3xl font-bold text-black-700 dark:text-gray-100">
+                    <span>{user ? getDisplayLabel(user) : "Login"}</span>
+
                 </span>
                 <div className="ml-auto flex items-center gap-3 pr-2 sm:gap-4">
                     <div className="flex items-center gap-2 sm:gap-4">
