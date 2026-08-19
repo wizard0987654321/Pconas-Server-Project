bd059ac9c78e12b71053e657a9777d9cd2da6425
Givi Sabashvili <givi.sabashvili@pcon.as>
2026-08-19 18:03:17 +0200

    small bugs fixed

--- frontend/src/components/pageContents/ServerRoom.tsx
+++ frontend/src/components/pageContents/ServerRoom.tsx
@@ -111,7 +111,7 @@ function ServerRoom() {
                                         />
 
                                         <div className="absolute font-mono left-1/2 top-full z-10 mt-2 hidden w-40 -translate-x-1/2 rounded-md border-3 border-[#6ADBAF] bg-[#F8FAFC] p-2 text-xs text-[#111827] shadow-lg group-hover:block dark:bg-[#0E1F48] dark:text-[#F9FAFB]">
-                                            <p><span className="font-semibold">{t("common.rack")}:</span> {rack.ID}</p>
+                                            <p><span className="font-semibold">{t("common.rack")}:</span> {rack.RackNumber}</p>
                                             <p><span className="font-semibold">{t("common.units")}:</span> {rack.UnitsSize} U</p>
                                             <p><span className="font-semibold">{t("common.height")}:</span> {rack.HeightCm} cm</p>
                                         </div>
